9e78a4a885b20290870676cc92017b64ad3b2969
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-25 13:46:31 -0600

    Precauciones de useEffect en componente de Message

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
 
+  const [ coords, setCoords ] = useState({
+    x: 0,
+    y: 0
+  });
+
   useEffect(() => {
-    console.log('Message Mounted');
+    // console.log('Message Mounted');
+
+    // Para limpiar el listener se debe de llamar antes, para
+    // despues poder cerrarlo
+    const onMouseMove = ({ x, y }) => {
+      // const coords = { x, y };
+      // console.log(coords);
+      setCoords({ x, y });
+    }
+
+    // Evento para saber donde esta el mouse o cursor
+    // window.addEventListener('mousemove', (event) => {
+    //   // console.log(event);
+    //   console.log(event.x, event.y);
+    // });
+
+    window.addEventListener('mousemove', onMouseMove);
 
     return () => {
-      console.log('Message Unmunted');
+      //console.log('Message Unmunted');
+
+      // Para limpiar un listener
+      window.removeEventListener('mousemove', onMouseMove)
     }
   }, [])
 
   return (
     <>
       <h3>Usuario ya existe</h3>
+
+      { JSON.stringify(coords) }
     </>
   );
 }
