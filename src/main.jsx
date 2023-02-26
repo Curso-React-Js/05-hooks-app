f89b02adeb096e90aaf27023a90ccfed9ea1a378
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-26 12:59:49 -0600

    Inicializando useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,11 @@ import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>,
-)
+// useReducer
+import'./08-useReducer/intro-reducer';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre />
+//   // </React.StrictMode>,
+// )
