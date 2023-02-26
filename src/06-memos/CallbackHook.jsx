85ca9501150b868f3ec861e6f259b3e8f44abf6a
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-26 12:23:19 -0600

    uso de useCallback otra manera para memorizar en react pero mas en funciones que cambie su valor

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
+export const CallbackHook = () => {
+
+  const [counter, setCounter] = useState(10);
+
+  const incrementFather = useCallback(() => {
+    // console.log('setCounter( counter + 1 );'); // Lo manda a llamar
+    // setCounter( counter + 1 );
+    setCounter( (value) => value + 1 );
+  }, []);
+
+  // Cada valor es diferente por eso no puede memorizarlo
+  // en el componente de ShowIncrement
+  // const incrementFather = () => {
+  //   setCounter( counter + 1 );
+  // }
+
+  // Otro uso, y no utilizamos el memo, lo volveria a redibujar
+  // se haria un ciclo infinito
+  useEffect(() => {
+    incrementFather();
+  }, [incrementFather]);
+  
+  return (
+    <>
+      <h1>useCallback Hook: { counter }</h1>
+      <hr />
+
+      <ShowIncrement increment={ incrementFather } />
+    </>
+  );
+}
