fea5f95cbafd835a3077a2c29f4e819e553cb8af
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-28 19:25:43 -0600

    Inicializando cascaron de un todo list con todoReducer basico

--- src/main.jsx
+++ src/main.jsx
@@ -14,12 +14,13 @@ import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 // useReducer
-import'./08-useReducer/intro-reducer';
+// import'./08-useReducer/intro-reducer';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Padre />
-//   // </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>,
+)
