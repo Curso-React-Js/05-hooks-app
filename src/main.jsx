0382153ac894b7d16104fe6c0e7c833e5c8be2aa
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-01 20:11:03 -0600

    Configurando routes y router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+
 import { HooksApp } from './HooksApp'
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -16,11 +18,37 @@ import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 import { TodoApp } from './08-useReducer/TodoApp'
 
+// Routes
+import { MainApp } from './09-useContext/MainApp'
+import { LoginPage } from './09-useContext/LoginPage'
+import { AboutPage } from './09-useContext/AboutPage'
+
 // useReducer
 // import'./08-useReducer/intro-reducer';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainApp />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/*',
+        element: <Navigate to='/about' />,
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <TodoApp />
+    <RouterProvider router={ router } />
   // </React.StrictMode>,
 )
