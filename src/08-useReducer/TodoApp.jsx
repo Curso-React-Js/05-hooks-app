8ccf934265dbff7510cb3b6ff5732cdf3d5b4db5
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-01 19:23:18 -0600

    uso de customHooks y refactorizacion

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,50 +1,15 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from './todoReducer';
 import { TodoList } from './TodoList';
 import { TodoAdd } from './TodoAdd';
 
-const initialState = [];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-}
+import { useTodo } from '../hooks';
 
 export const TodoApp = () => {
 
-  const [ todos, dispatchTodo ] = useReducer(todoReducer, initialState, init);
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [ todos ])
-
-  const handleNewTodo = ( todo ) => {
-    const action = {
-      type: '[TODO] Add Todo',
-      payload: todo
-    }
-
-    dispatchTodo( action );
-  }
-
-  const handleDeleteTodo = ( id ) => {
-    const action = {
-      type: '[TODO] Remove Todo',
-      payload: id
-    }
-
-    dispatchTodo( action );
-  }
-
-  const onToggleTodo = ( id ) => {
-    dispatchTodo({
-      type: '[TODO] Toggle Todo',
-      payload: id
-    });
-  }
-
+  const { todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, onToggleTodo } = useTodo();
+ 
   return (
     <>
-      <h1>TodoApp: ({ todos.length }), <small>pendientes: 2</small></h1>
+      <h1>TodoApp: ({ todosCount }), <small>pendientes: { pendingTodosCount }</small></h1>
 
       <hr />
 
