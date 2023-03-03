4ad74e01db80cd0bd298b24544445a9e934e7076
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-02 18:44:27 -0600

    Uso de Link para no refrescar pagina al cambiar de sitio

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,12 +1,19 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+
 import { HomePage } from './HomePage';
 import { AboutPage } from './AboutPage';
 import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
 
+// Link es para no hacer refresh de la pagina
+// Doc: https://reactrouter.com/en/main/start/tutorial
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+
+      <Navbar />
+
       <hr />
 
       <Routes>
