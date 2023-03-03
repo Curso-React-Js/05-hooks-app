a4ea94d613d4c666ddc75fc8b595e4c9d111e03f
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-02 19:34:33 -0600

    uso de un useContext para pasar informacion a diferentes componentes

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-  id: 123,
-  name: 'Angel FG',
-  email: 'angefg@google.com'
-}
+// const user = {
+//   id: 123,
+//   name: 'Angel FG',
+//   email: 'angefg@google.com'
+// }
 
 export const UserProvider = ({ children }) => {
+
+  const [ user, setUser ] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: 'Mundo', user }}>
+    // <UserContext.Provider value={{ hola: 'Mundo', user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       { children }
     </UserContext.Provider>
   );
