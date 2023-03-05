35f1abdf2245e86de8187f07d925fb1b6bd9e510
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-05 14:21:37 -0600

    Pruebas de TodoApp de forma general

---
+++ test/08-useReducer/TodoApp.test.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { TodoApp } from '../../src/08-useReducer/TodoApp';
+import { useTodo } from '../../src/hooks/useTodo';
+
+jest.mock('../../src/hooks/useTodo');
+
+describe('Pruebas en <TodoApp />', () => {
+
+  useTodo.mockReturnValue({
+    todos: [
+      { id: 1, description: 'Todo #1', done: false },
+      { id: 2, description: 'Todo #2', done: true },
+    ], 
+    todosCount: 2, 
+    pendingTodosCount: 1, 
+    handleNewTodo: jest.fn(),
+    handleDeleteTodo: jest.fn(), 
+    onToggleTodo: jest.fn(),
+  });
+  
+  test('debe de mostrar el componente correctamente', () => {
+    
+    render(<TodoApp />);
+    // screen.debug();
+
+    expect( screen.getByText('Todo #1') ).toBeTruthy();
+    expect( screen.getByText('Todo #2') ).toBeTruthy();
+    expect( screen.getByRole('textbox') ).toBeTruthy();
+
+    // console.log(screen.getByRole('textbox').innerHTML);
+    // console.log(screen.getByRole('textbox').name); // description
+    console.log(screen.getByRole('textbox').className); // form-control
+
+  });
+  
+});
